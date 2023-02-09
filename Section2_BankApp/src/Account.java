cfcbb9c9b828bb93cf1109dd45eb0fb672af4425
owenherrera0326 <owenherrera0326@gmail.com>
2023-02-09 17:08:16 -0600

    New code for class on friday

--- Section2_BankApp/src/Account.java
+++ Section2_BankApp/src/Account.java
@@ -2,23 +2,23 @@
 public class Account {
 	
 	// this is a class level field - share across all accounts
-	public static int lastNumber = 0;
+	private static int lastNumber = 0;
 	
 	// instance level fields - unique per account
 	// a field can also be called attribute or property
-	public int number;
-	public String firstName;
-	public String lastName;
-	public double balance;
+	private int number;
+	private String firstName;
+	private String lastName;
+	private double balance;
 	
 	// default constructor that will return a unique account object 
 	public Account() {
 		System.out.println("Default constructor");
 		
-		this.number = ++Account.lastNumber;
-		this.balance = 0;
-		this.firstName = "Unknown";
-		this.lastName = "Unknown";
+		this.number 	= ++Account.lastNumber;
+		this.balance 	= 0;
+		this.firstName 	= "Unknown";
+		this.lastName	= "Unknown";
 	} // end of default
 
 	// overload constructor that allows setting the object's fields
@@ -28,9 +28,43 @@ public class Account {
 		// can not run this before calling default constructor
 		System.out.println("Overload constructor");
 		
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.balance = balance;
+		this.firstName 	= firstName;
+		this.lastName 	= lastName;
+		this.balance 	= balance;
 	} // end of overload constructor
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
 	
 } // end of Account class
--- Section2_BankApp/src/BankApp.java
+++ Section2_BankApp/src/BankApp.java
@@ -2,9 +2,9 @@ public class BankApp {
 
 	public static void printAccount(Account account) {
 		System.out.println("Ref Val: " + account);
-		System.out.println("Account: " + account.number);
-		System.out.println("Name:    " + account.firstName + " " + account.lastName); 
-		System.out.println("Balance: " + account.balance);
+		System.out.println("Account: " + account.getNumber());
+		System.out.println("Name:    " + account.getFirstName() + " " + account.getLastName()); 
+		System.out.println("Balance: " + account.getBalance());
 		System.out.println();
 	}
 
@@ -15,17 +15,19 @@ public class BankApp {
 		BankApp.printAccount(account1);
 
 		// Change the instance's field values
-		account1.balance = 1000.10;
-		account1.firstName = "Debbie";
-		account1.lastName = "Johnson";
+		account1.setBalance(1000.10);
+		account1.setFirstName("Debbie");
+		account1.setLastName("Johnson");
 		BankApp.printAccount(account1);
 		
 		// Instantiate another new Account object using the overload constructor
 		Account account2 = new Account("Sam", "Smith", 2000.20);
 		BankApp.printAccount(account2);
 		
+		/*
 		// Displaying class level field's value
 		System.out.println("Class level field lastNumber: " + Account.lastNumber);
+		*/
 
 	} // end of main
 	
